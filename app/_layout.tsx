1dd2ef2e83b294dcd07cbf0337967511261dd722
Menushaa <menushaav.21@uom.lk>
2024-12-06 00:03:37 +0530

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,37 @@
+import FontAwesome  from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/components/useColorScheme';
+
+export{
+  ErrorBoundary,
+} from 'expo-router';
+
+
+export const unstable_setting={
+  initialRouteName:'(tabs)', 
+};
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+    const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
   });
 
+  
+  useEffect(()=>{
+    if (error) throw error;
+  },[error]);
+  
+  
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -26,14 +41,13 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
+  return <RootLayoutNav />;
+}
 
+function RootLayoutNav(){
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
